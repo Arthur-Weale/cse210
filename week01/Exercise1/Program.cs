85a466b690844354a50b4584181e09c6e3b30e55
Arthur Weale <ajayrossy340@gmail.com>
2025-03-02 16:55:32 +0200

    Refactored code , well sort of

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name ?"); //Console.Write is the equivalen to input in python , This line of code asks for user input.
+        Console.Write("What is your first name?"); //Console.Write is the equivalen to input in python , This line of code asks for user input.
         string userName = Console.ReadLine(); //Console.ReadLine is the equivalent to print in python , this prints in the user input.
-        Console.Write("What is your last name ?");
+        Console.Write("What is your last name?");
         string userLastName = Console.ReadLine();
 
         Console.WriteLine($"Your name is {userLastName}, {userName} {userLastName}.");
