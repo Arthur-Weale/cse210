4a41b6538d0b0676f787bba93c543e62c05c76bb
Arthur Weale <ajayrossy340@gmail.com>
2025-03-02 16:51:17 +0200

    Added comments

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,8 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name ?");
-        string userName = Console.ReadLine();
+        Console.Write("What is your first name ?"); //Console.Write is the equivalen to input in python , This line of code asks for user input.
+        string userName = Console.ReadLine(); //Console.ReadLine is the equivalent to print in python , this prints in the user input.
         Console.Write("What is your last name ?");
         string userLastName = Console.ReadLine();
 
