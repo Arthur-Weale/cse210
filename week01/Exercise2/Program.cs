1853c7f97e405ba894a62a1858cab6a177563c01
Arthur Weale <ajayrossy340@gmail.com>
2025-03-02 18:40:55 +0200

    Exercise Complete

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage?");
+        string grade = Console.ReadLine();
+        int intGrade = int.Parse(grade);
+
+        string letter = "";
+        if (intGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (intGrade >= 80)
+        {
+            letter = "B";
+        }
+        else if (intGrade >= 70)
+        {
+            letter = "C";
+        }
+        else if (intGrade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}.");
+        if (intGrade >= 70)
+            {
+                Console.WriteLine("Congratulations! You passed.");
+            }
+        else
+            {
+                Console.WriteLine("Sorry, Study smarter not harder.");
+            }
     }
 }
