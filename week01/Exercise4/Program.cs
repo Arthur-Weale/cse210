941558268027143002bdb5fa2e05b618a2d4989f
Arthur Weale <ajayrossy340@gmail.com>
2025-03-03 14:49:22 +0200

    Exercise Complete

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        List<int> numList = new List<int>();//Creates a list.
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int userInputNum;
+        int runningTotal = 0;
+        int avg = 0;
+        int max;
+        do
+            {
+                Console.Write("Enter number: ");
+                string userInput = Console.ReadLine();
+                userInputNum = int.Parse(userInput);
+                
+                if(userInputNum > 0)
+                    {
+                        numList.Add(userInputNum); //Adds objects in the list
+                    }
+
+                max =  numList.Max(); //Finds the largest number in a list
+                if (userInputNum == 0) //Condition to start making calculations if the input is zero
+                    {
+                        foreach (int integer in numList)
+                            {
+                                //Mathematical operations perfomed below.
+                                runningTotal = runningTotal+ integer; 
+                                avg = runningTotal / numList.Count();
+                            }
+                            Console.WriteLine($"The sum is: {runningTotal}");
+                            Console.WriteLine($"The average is: {avg}");
+                            Console.WriteLine($"The largest is: {max}");
+                    }
+                
+            }
+        while(userInputNum != 0);
     }
 }
