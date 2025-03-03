959199f1fbf22e40a07d8bca09eb2658bf7c43e0
Arthur Weale <ajayrossy340@gmail.com>
2025-03-03 13:12:28 +0200

    Exercise complete

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random(); //With the Random class, creates a storage for the random numbers
+        int number = randomGenerator.Next(1,101);// With the method next , this allows creation of non negative numbers from 0
+        int guessNum ;
+
+        //Loop 
+        do{ 
+            Console.WriteLine("Guess the random number:");
+            string userGuess = Console.ReadLine();
+            guessNum = int.Parse(userGuess); //Changes a string to integer
+            
+                //Checks if the guess number is greater than the generated number
+                if (guessNum > number)
+                    {
+                        Console.WriteLine("You need to guess lower.");
+                    }
+                else if (guessNum < number) //Checks if the guess number is less than the generated number
+                    {
+                        Console.WriteLine("You need to guess higher.");
+                    }
+
+            }while (guessNum != number); //Makes the loop to execute continuously as long as the guess number and the random generated number are not equal
+            
+            Console.WriteLine("Your Guess is correct. Congratulations!");
     }
 }
