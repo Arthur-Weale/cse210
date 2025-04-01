b28ffa35b82077820545723483cfbf43c12e822f
Arthur Weale <ajayrossy340@gmail.com>
2025-04-01 23:07:22 +0200

    Assignment Complete, had lots of fun doing this

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignments assignments = new Assignments("Arthur Weale", "Multiplication");
+        Console.WriteLine(assignments.GetSummary());
+        Console.WriteLine();
+
+
+        // Assignments assignments1 = new Assignments("Arthur Weale", "Multiplication");
+        // Console.WriteLine(assignments1.GetSummary());
+        MathAssignment mathAssignment = new MathAssignment("7.3", "8-19", "Arthur Weale", "Multiplication");
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment writingAssignment = new WritingAssignment("The Causes of World War II by " ,"Arthur Weale", "European History");
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
