e93712bf607f1305f6fb2a964ca28d2d8215be44
Arthur Weale <ajayrossy340@gmail.com>
2025-04-02 04:01:06 +0200

    Created the first program functionality

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        
+        
+        int userInputInt ;
+        int durationTime;
+
+        do{
+            Console.WriteLine("Menu Options");
+            Console.WriteLine("1.Start Breathing Activity");
+            Console.WriteLine("2.Start Reflecting Activity");
+            Console.WriteLine("3.Start Listing Activity");
+            Console.WriteLine("4.Quit");
+            Console.Write("Select a choice from the Menu: ");
+            string userInput = Console.ReadLine();
+            userInputInt = int.Parse(userInput);
+            
+
+            if (userInputInt == 1)
+            {
+                Activity activity = new Activity("Breathing", "help you relax by walking your breathing in and out slowly. Clear your mind and focus your breathing");
+                activity.DisplayStartMessage();
+                string duration = Console.ReadLine();
+                durationTime = int.Parse(duration);
+
+                activity.SetDuration(durationTime);
+            }
+        }
+        while(userInputInt != 4);
+
+        
     }
 }
