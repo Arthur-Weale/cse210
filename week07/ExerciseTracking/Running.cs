0429c85e5c034dd450d07eba686c99369a71a47c
Arthur Weale <ajayrossy340@gmail.com>
2025-04-16 19:18:09 +0200

    Complete

---
+++ week07/ExerciseTracking/Running.cs
@@ -0,0 +1,19 @@
+using System;
+namespace ExerciseTracking;
+
+public class Running : Activity
+{
+    private double _distanceInKm;
+
+    public Running(DateTime date, int lengthInMinutes, double distanceInKm)
+        : base(date, lengthInMinutes)
+    {
+        _distanceInKm = distanceInKm;
+    }
+
+    public override double GetDistance() => _distanceInKm;
+
+    public override double GetSpeed() => (_distanceInKm / LengthInMinutes) * 60;
+
+    public override double GetPace() => LengthInMinutes / _distanceInKm;
+}
---
+++ week07/ExerciseTracking/Swimming.cs
@@ -0,0 +1,18 @@
+using System;
+namespace ExerciseTracking;
+public class Swimming : Activity
+{
+    private int _laps;
+
+    public Swimming(DateTime date, int lengthInMinutes, int laps)
+        : base(date, lengthInMinutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance() => (_laps * 50) / 1000.0;
+
+    public override double GetSpeed() => (GetDistance() / LengthInMinutes) * 60;
+
+    public override double GetPace() => LengthInMinutes / GetDistance();
+}
